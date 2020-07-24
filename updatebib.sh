d4c62c4a8a749725f6d65cef48d3eca915410b88
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2020-07-23 21:18:52 -0400

    edit script

--- updatebib.sh
+++ updatebib.sh
@@ -3,7 +3,10 @@
 pushd ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley
 ./populate_additional_libraries.sh
 popd
-cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library*.bib .
+cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library-no-url-short.bib .
+cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library-no-url.bib .
+cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library-short.bib .
+cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library.bib .
 
 git pull
 git add -f library-no-url.bib library.bib library-no-url-short.bib library-short.bib

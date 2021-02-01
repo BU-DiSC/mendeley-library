98bbcb06493434565404fc0f2e049824cafa0212
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2021-02-01 18:54:46 -0500

    update script, on: updatebib.sh

--- updatebib.sh
+++ updatebib.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+git pull
 pushd ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley
 ./populate_additional_libraries.sh
 popd
@@ -8,7 +9,6 @@ cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library-no-u
 cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library-short.bib .
 cp ~/Dropbox/Professional/Professorship/W-Lab/Bibliography-Mendeley/library.bib .
 
-git pull
 git add -f library-no-url.bib library.bib library-no-url-short.bib library-short.bib
 git commit -m "auto updating bib library"
 git push
